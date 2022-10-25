ca8b57fca45b523dea3f9418a97e99dc28db9566
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2022-10-25 10:49:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNum
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("\nEnter a number (Positive): ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine($"{num} is not a valid number");
+                // Console.ReadKey();
+                return;
+            }
+            if (num <= 2)
+            {
+                Console.WriteLine($"{num} is not a prime number");
+                // Console.ReadKey();
+                return;
+            }
+            else
+            {
+                for (int i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        Console.WriteLine($"{num} is not a prime number");
+                        //Console.ReadKey();
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{num} is a prime number");
+                        // Console.ReadKey();
+                        return;
+                    }
+
+                }
+
+            }
+
+
+            // Console.ReadKey();
+
+
+        }
+    }
+}
